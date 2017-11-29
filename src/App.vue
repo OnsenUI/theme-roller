edc7b1983b5cd1b3bfb3939e154791934dcc2314
Fran Dios <frankdiox@gmail.com>
2017-11-29 22:04:57 +0900

    Add Axios and Ancss.

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,19 @@
 </template>
 
 <script>
+import ancss from 'ancss';
+import axios from 'axios';
+
 export default {
   name: 'App',
+  mounted() {
+    axios
+      .get('https://unpkg.com/onsenui@2.8.2/css/onsen-css-components.css')
+      .then((response) => {
+        const components = ancss.parse(response.data, { detect: line => line.match(/^~/) });
+        console.log(components);
+      });
+  },
 };
 </script>
 
