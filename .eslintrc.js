dbeae429d9368970f2ed4c49a87b84264ff4c6ae
Fran Dios <frankdiox@gmail.com>
2017-11-29 20:09:34 +0900

    Vue project with new Vue ESLint rules.

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+  },
+  extends: ['airbnb-base', 'plugin:vue/recommended'],
+  // check if imports actually resolve
+  'settings': {
+    'import/resolver': {
+      'webpack': {
+        'config': 'build/webpack.base.conf.js'
+      }
+    }
+  },
+  // add your custom rules here
+  'rules': {
+    // don't require .vue extension when importing
+    'import/extensions': ['error', 'always', {
+      'js': 'never',
+      'vue': 'never'
+    }],
+    // allow optionalDependencies
+    'import/no-extraneous-dependencies': ['error', {
+      'optionalDependencies': ['test/unit/index.js']
+    }],
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+
+    // VUE
+
+    'vue/max-attributes-per-line': [2, {
+      'singleline': 3,
+      'multiline': {
+        'max': 1,
+        'allowFirstLine': false
+      }
+    }]
+  }
+}
---
+++ README.md
@@ -0,0 +1,29 @@
+# theme-roller
+
+> Onsen UI Theme Roller
+
+## Build Setup
+
+``` bash
+# install dependencies
+yarn install
+
+# serve with hot reload at localhost:8080
+yarn run dev
+
+# build for production with minification
+yarn run build
+
+# build for production and view the bundle analyzer report
+yarn run build --report
+
+# run unit tests
+yarn run unit
+
+# run e2e tests
+yarn run e2e
+
+# run all tests
+yarn test
+```
+
