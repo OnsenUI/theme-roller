f012fd82ea619f3da015652794f4dc2ace686732
Fran Dios <frankdiox@gmail.com>
2017-12-09 02:05:53 +0900

    Add tabs to side menu.

---
+++ src/components/TRGenerator.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="tr-generator">
+    <div class="tr-generator__button">
+      <button
+        @click="generate"
+      >
+        Generate and download
+      </button>
+    </div>
+
+    <div class="tr-generator__message">
+      You can also remove the components you don't
+      you need from your custom CSS stylesheet.
+      What should be included?
+    </div>
+
+    <ul class="tr-generator__components">
+      <li
+        v-for="component in fullComponentsList"
+        :key="`${version}-${component}`"
+      >
+        <label>
+          <a>
+            <input
+              type="checkbox"
+              :value="component"
+              v-model="selectedComponents"
+            >
+            <span>{{ component }}</span>
+          </a>
+        </label>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import CSSProcessor from '@/css-processor';
+import { mapMutationState } from '@/store';
+import util from '@/util';
+
+const re = /(global|util|combination)/i;
+
+export default {
+  name: 'TRGenerator',
+  data() {
+    return {
+      components: null,
+    };
+  },
+  computed: {
+    ...mapMutationState([
+      'customComponentsIndex',
+      'fullComponentsIndex',
+      'rootCSS',
+      'theme',
+      'version',
+    ]),
+    fullComponentsList() {
+      return (this.fullComponentsIndex
+        .match(/'([./-\w]+)\.css'/img) || [])
+        .filter(m => !re.test(m))
+        .map(m => util.toLabel(m.slice(3, -5)))
+        .sort();
+    },
+    selectedComponents: {
+      get() {
+        return this.components
+          ? this.components
+          : this.fullComponentsList;
+      },
+      set(value) {
+        this.components = value;
+      },
+    },
+  },
+
+  methods: {
+    generate() {
+      const includedComponents = this.selectedComponents
+        .map(c => `${util.toId(c)}.css`);
+
+      const index = this.fullComponentsIndex
+        .split('\n')
+        .filter(line => re.test(line)
+          || includedComponents.some(c => line.indexOf(c) !== -1))
+        .join('\n');
+
+      const precss = CSSProcessor
+        .replace(this.rootCSS, this.theme, index);
+
+      CSSProcessor
+        .compile(precss)
+        .then((css) => {
+          this.$log(css);
+          alert('Check the console logs.'); // eslint-disable-line
+        });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.tr-generator {
+
+}
+.tr-generator__message {
+  margin-top: 10px;
+  padding: var(--content-padding);
+  padding-bottom: 0;
+}
+
+.tr-generator__button {
+  margin: 10px var(--content-padding);
+}
+
+.tr-generator__button button {
+  width: 100%;
+  text-align: center;
+}
+
+.tr-generator__components {
+  padding: 0 40px;
+}
+
+.tr-generator__components label {
+  margin-top: 10px;
+  display: block;
+  width: 100%;
+  height: 26px;
+  line-height: 26px;
+  cursor: pointer;
+}
+
+.tr-generator__components li:nth-child(even) {
+  background-color: #f0f0f0;
+}
+</style>
--- src/components/TRSide.vue
+++ src/components/TRSide.vue
@@ -1,69 +1,112 @@
 <template>
   <div class="tr-side">
-    <div>Components List</div>
-    <button @click="refresh">Refresh</button>
-    <ul>
-      <li
-        v-for="component in componentsList"
-        :key="`${version}-${component}`"
-      >
-        <label>
-          <input
-            type="checkbox"
-            :value="component"
-            v-model="selectedComponents"
-          >
-          <span>{{ component }}</span>
-        </label>
-      </li>
-    </ul>
+    <div class="tr-side__tabs">
+      <ul>
+        <li
+          v-for="(tab, $index) in tabs"
+          :key="tab.label"
+        >
+          <label>
+            <input
+              type="radio"
+              style="display:none"
+              :value="$index"
+              v-model="tabActive"
+            >
+            <a class="tr-side__tab">
+              <span>{{ tab.label }}</span>
+            </a>
+          </label>
+        </li>
+      </ul>
+    </div>
+
+    <div class="tr-side__content">
+      <keep-alive>
+        <component :is="tabs[tabActive].component" />
+      </keep-alive>
+    </div>
   </div>
 </template>
 
 <script>
-import { mapMutationState } from '@/store';
-import util from '@/util';
-
-const re = /(global|util|combination)/i;
+import TRGenerator from '@/components/TRGenerator';
 
 export default {
-  name: 'Placeholder',
+  name: 'TRSide',
   data() {
     return {
-      selectedComponents: [],
+      tabActive: 0,
+      tabs: [
+        {
+          label: 'View',
+          component: null,
+        },
+        {
+          label: 'Editor',
+          component: null,
+        },
+        {
+          label: 'Download',
+          component: TRGenerator,
+        },
+      ],
     };
-  },
-  computed: {
-    ...mapMutationState(['version', 'fullComponentsIndex', 'customComponentsIndex']),
-    componentsList() {
-      return (this.fullComponentsIndex
-        .match(/'([./-\w]+)\.css'/img) || [])
-        .filter(m => !re.test(m))
-        .map(m => util.toLabel(m.slice(3, -5)))
-        .sort();
-    },
-  },
-
-  watch: {
-    componentsList(value) {
-      this.selectedComponents = [...value];
-    },
-  },
-
-  methods: {
-    refresh() {
-      const includedComponents = this.selectedComponents.map(c => `${util.toId(c)}.css`);
-      this.customComponentsIndex = this.fullComponentsIndex
-        .split('\n')
-        .filter(line => re.test(line) || includedComponents.some(c => line.indexOf(c) !== -1))
-        .join('\n');
-    },
   },
 };
 </script>
 
 <style scoped>
 .tr-side {
+  border-right: 1px solid var(--border-color);
+}
 
+.tr-side__tabs {
+  user-select: none;
+  align-items: stretch;
+  display: flex;
+  font-size: 1rem;
+  justify-content: space-between;
+  overflow: hidden;
+  overflow-x: auto;
+  white-space: nowrap;
+}
+
+.tr-side__tabs ul {
+  align-items: center;
+  border-bottom-color: var(--border-color);
+  border-bottom-style: solid;
+  border-bottom-width: 1px;
+  display: flex;
+  flex-grow: 1;
+  flex-shrink: 0;
+  justify-content: flex-start;
+  padding-left: 20px;
+}
+
+.tr-side__tabs li {
+  display: block;
+}
+
+.tr-side__tab {
+  align-items: center;
+  border-bottom-color: var(--border-color);
+  border-bottom-style: solid;
+  border-bottom-width: 1px;
+  color: #4a4a4a;
+  display: flex;
+  justify-content: center;
+  margin-bottom: -2px;
+  padding: 0.5em 1em;
+  vertical-align: top;
+}
+
+:checked + .tr-side__tab {
+  border: 1px solid transparent;
+  border-radius: 3px 3px 0 0;
+
+  background-color: white;
+  border-color: var(--border-color);
+  border-bottom-color: transparent !important;
 }
 </style>
