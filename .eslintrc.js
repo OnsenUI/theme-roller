301783c70113caf2b6d8c30d07fce56ab63bc7a5
Fran Dios <frankdiox@gmail.com>
2018-01-10 16:28:14 +0900

    Add HTTP retry.

--- .eslintrc.js
+++ .eslintrc.js
@@ -50,6 +50,7 @@ module.exports = {
           'state',
           'acc',
           'e',
+          'error',
           'ctx',
           'req',
           'request',
--- src/api/request.js
+++ src/api/request.js
@@ -1,6 +1,17 @@
 import axios from 'axios';
 import cache from '@/api/cache';
 
+axios.interceptors.response.use(undefined, (error) => {
+  // Unpkg sometimes does not have files cached
+  if (error.response.status === 404 && error.config && !error.config.isRetry) {
+    error.config.isRetry = true;
+    console.info('HTTP error - Retrying after 3 seconds.'); // eslint-disable-line
+    return new Promise(resolve => setTimeout(() => resolve(axios(error.config)), 3000));
+  }
+
+  return Promise.reject(error);
+});
+
 export default function request(uri, {
   filter = res => res,
   headers = { 'Content-Type': 'application/json' },
